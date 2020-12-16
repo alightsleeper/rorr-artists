74ed55e1880e17d73e94983d7396f5ebcdc0f562
alightsleeper <alightsleeper@gmail.com>
2020-12-16 17:09:20 -0600

    add home view and links

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Link
+ } from 'react-router-dom'
 import Artists from './Artists/Artists'
 import Artist from './Artists/Artist'
 import Venues from './Venues/Venues'
@@ -7,12 +12,17 @@ import Venue from './Venues/Venue'
 
 const App = () => {
     return (
-        <Switch>
-            <Route exact path="/artists" component={Artists}/>
-            <Route exact path="/artists/:id" component={Artist}/>
-            <Route exact path="/venues" component={Venues}/>
-            <Route exact path="/venues/:id" component={Venue}/>
-        </Switch>
+        <Router>
+            <Link to="/">Home</Link>
+            <Link to="/artists">Artists</Link>
+            <Link to="/venues">Venues</Link>
+            <Switch>
+                <Route exact path="/artists" component={Artists}/>
+                <Route exact path="/artists/:id" component={Artist}/>
+                <Route exact path="/venues" component={Venues}/>
+                <Route exact path="/venues/:id" component={Venue}/>
+            </Switch>
+        </Router>
     )
 }
 
