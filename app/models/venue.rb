a62531e3d7e7b435a29e59b83057e817da8b8d53
alightsleeper <alightsleeper@gmail.com>
2020-12-24 11:56:01 -0600

    add venue spec and validation

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,6 +2,7 @@ class Venue < ApplicationRecord
     before_create :slugify
     has_many :reviews
     has_many :performances
+    validates :name, presence: true, uniqueness: { case_sensitive: false } 
 
     def slugify
         self.slug = name.parameterize
---
+++ spec/models/venue_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Venue, :type => :model do
+    it "is valid with valid attributes" do
+        expect(Venue.new(
+            name: "Sir Digby Chicken Caesar", 
+        )).to be_valid
+    end
+
+    it "is not valid without a name" do
+        expect(Venue.new).to_not be_valid
+    end
+
+    it "is not valid if name already exists" do
+        Venue.new(name: "Sir Digby Chicken Caesar").save
+
+        expect(Venue.new(
+            name: "Sir Digby Chicken Caesar", 
+        )).to_not be_valid
+
+        expect(Venue.new(
+            name: "sir digby chicken caesar", 
+        )).to_not be_valid
+    end
+end
